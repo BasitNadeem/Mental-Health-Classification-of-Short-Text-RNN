d662259bfd72c257e65d94320f7eb5d035a6b0cf
BasitNadeem <basit.nadeem5@gmail.com>
2022-10-21 01:34:57 +0500

    py scripts

---
+++ clean_data.py
@@ -0,0 +1,289 @@
+import pandas as pd
+import numpy as np
+import re
+import warnings
+warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
+import get_tree as gt
+import igraph as ig
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+import random
+from tqdm import tqdm
+import argparse
+
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        os.makedirs(path)
+        print("---  new folder...  ---")
+        print("---  OK  ---")
+    else:
+        print("---  There is this folder!  ---")
+
+
+# clear text
+def preprocess_text(sen):
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', str(sen))
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+    return sentence.lower()
+
+
+def tag_process(tag):
+    # Remove punctuations and numbers
+    # sentence = re.sub('[^a-zA-Z]', ' ', str(tag))
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', str(tag))
+    return sentence.lower()
+
+
+def x_write(list_name, list_to_file_name):
+    with open(list_to_file_name, 'a+') as f:
+        for line_value in list_name:
+            f.write(str(line_value) + "\n")
+
+
+def y_write(list_name, list_to_file_name):
+    with open(list_to_file_name, 'a+') as f:
+        for line_value in list_name:
+            s = ""
+            for i in range(len(line_value)):
+                if i != len(line_value) - 1:
+                    s += str(int(line_value[i]))
+                    s += " "
+                else:
+                    s += str(int(line_value[i]))
+                    s += "\n"
+            f.write(s)
+
+
+def show_X_train_info(X_train):
+    """
+    show the min max and avg length of the text
+    :param y_train:
+    :return:
+    """
+    length = []
+    for tweet in X_train:
+        length.append(len(tweet.split(" ")))
+    max_len = np.max(length)
+    min_len = np.min(length)
+    mean_len = np.mean(length)
+    print("the min length of train text is: ")
+    print(min_len)
+    print("\n")
+
+    print("the mean length of train text is: ")
+    print(mean_len)
+    print("\n")
+
+    print("the max length of train text is: ")
+    print(max_len)
+    print("\n")
+
+
+def split_data(meta_data, label_name, min_label_num):
+    """
+    drop the labels not in label_name and drop the data with len(label) less than 2
+    :param meta_data: the input the pd data
+    :param label_name: the top k label name
+    :param min_label_num: minimum number of labels per instance
+    :return:
+    """
+    #
+    random.seed(0)
+    threshold = 0.1
+    #
+    X_test = []
+    X_train = []
+    y_train = []
+    y_test = []
+    y_test_text = []
+    y_train_text = []
+    for index, row in tqdm(meta_data.iterrows()):
+        # drop the data with num of labels less than two
+        tags = str(row["Tags_clean"]).split(",")
+        tags_clean = []
+        tags_clean_binary = [0 for i in range(len(label_name))]
+        for tag in tags:
+            if tag in label_name:
+                tags_clean.append(tag)
+                tags_clean_binary[label_name.index(tag)] = 1
+        if len(tags_clean) >= min_label_num:
+            if random.random() > threshold:
+                X_train.append(row["Title_clean"])
+                y_train.append(tags_clean_binary)
+                y_train_text.append(tags_clean)
+            else:
+                X_test.append(row["Title_clean"])
+                y_test.append(tags_clean_binary)
+                y_test_text.append(tags_clean)
+    # delete the all 0s columns
+    new_label_name = []
+    label_sum = list(np.sum(y_train, axis=0))
+    for i in range(len(label_sum)):
+        if label_sum[i] == 0:
+            for row in y_train:
+                del row[i]
+            for row in y_test:
+                del row[i]
+        else:
+            new_label_name.append(label_name[i])
+    # print the info of train text
+    show_X_train_info(X_train)
+    # write to processed/archive
+    x_write(X_test, "data/processed/X_test" + ".txt")
+    x_write(X_train, "data/processed/X_train" + ".txt")
+    y_write(y_train, "data/processed/y_train" + ".txt")
+    y_write(y_test, "data/processed/y_test" + ".txt")
+    return y_train, new_label_name
+
+
+def remove_duplicate(file_name):
+    """
+    drop the duplicates and process the text
+    :param file_name:
+    :return: no repeat clean file
+    """
+    # remove duplicates
+    df = pd.read_csv(file_name + ".csv", sep=",", names=["Title", "Tags"])
+    duplicate_pairs = df.duplicated('Title')
+    print("Total number of duplicate questions : ", duplicate_pairs.sum())
+    df = df[~duplicate_pairs]
+    print("Dataframe shape after duplicate removal : ", df.shape)
+    # process the tags
+    df["Title_clean"] = df["Title"].apply(lambda x: preprocess_text(x))
+    df["Tags_clean"] = df["Tags"].apply(lambda x: tag_process(x))
+    df_clean = df.drop(columns=["Title", "Tags"])
+    df_clean.to_csv(file_name + "_no_repeat" + ".csv")
+    print("write done")
+    return df_clean
+
+
+def get_frequent_labels(df, topk):
+    """
+    return the top frequent labels
+    :param df: clean no repeat data
+    :param topk: top k frequent labels
+    :return: top 20 frequent labels
+    """
+    vectorizer = CountVectorizer(tokenizer=lambda x: x.split(","))
+    tag_bow = vectorizer.fit_transform(df['Tags_clean'].values.astype(str))
+    # show info
+    print("Number of questions :", tag_bow.shape[0])
+    print("Number of unique tags :", tag_bow.shape[1])
+    tags = vectorizer.get_feature_names()
+    freq = tag_bow.sum(axis=0).A1
+    tag_to_count_map = dict(zip(tags, freq))
+    list = []
+    for key, value in tag_to_count_map.items():
+        list.append([key, value])
+    tag_df = pd.DataFrame(list, columns=['Tags', 'Counts'])
+    tag_df_sorted = tag_df.sort_values(['Counts'], ascending=False)
+    i = np.arange(topk)
+    tag_df_sorted.head(topk).plot(kind='bar')
+    plt.title('Frequency of top k tags')
+    plt.xticks(i, tag_df_sorted['Tags'])
+    plt.xlabel('Tags')
+    plt.ylabel('Counts')
+    plt.savefig("data/img/topk.png")
+    return tag_df_sorted["Tags"][:topk].values.tolist()
+
+
+def show_tree(y_train, label_name, threshold, steps, score_method):
+    """
+    show the tree structure
+    :param y_train: the y training data
+    :param label_name: the label name to be shown in the tree img
+    :param threshold: edge weight bigger than the threshold will be kept
+    :param steps: the steps max-spanning tree algorithm working
+    :param score_method: method to calculate the correlations
+    :return: no
+    """
+    # use the true name of the labels
+    tree_node, tree_graph, tree_edge = gt.get_tree(y_train, label_name, threshold, steps, score_method)
+    layout = tree_graph.layout_lgl()
+    ig.drawing.plot(tree_graph, "data/tree_img/tree_" + str(score_method) + ".png", layout=layout, bbox=(850, 850),
+                    margin=(80, 80, 80, 80))
+    # use the int number to represent the labels
+    label_name_digit = [str(i) for i in range(len(label_name))]
+    tree_node_digit, tree_graph_digit, tree_edge_digit = gt.get_tree(y_train, label_name_digit, threshold, steps, score_method)
+    layout_digit = tree_graph_digit.layout_lgl()
+    ig.drawing.plot(tree_graph_digit, "data/tree_img/tree_" + str(score_method) + "_digit.png", layout=layout_digit, bbox=(850, 850),
+                    margin=(80, 80, 80, 80))
+    # mkdir data/store/edges
+    for edge in tree_edge_digit:
+        edge_dir = "data/store/" + "l" + str(edge[0]) + "_l" + str(edge[1])
+        mkdir(edge_dir)
+
+
+def mkdir_necessary():
+    """
+    mkdir the necessary folder
+    :return: no
+    """
+    # mkdir necessary directories
+    mkdir("data")
+    mkdir("data/processed")
+    mkdir("data/tree_img")
+    mkdir("data/img")
+    mkdir("data/store")
+
+
+def show_y_train_info(y_train):
+    row_distribution = []
+    for i in range(len(y_train)):
+        row_distribution.append(np.sum(y_train[i]))
+    row_mean = np.mean(row_distribution)
+    print("the avg number of the labels per instance in the train is: ")
+    print(row_mean)
+    print("\n")
+
+
+def data_clean(file_name, topk, min_avg_labels, correlation_method, threshold):
+    """
+    clean the data and generate the tree structure
+    :param file_name:
+    :param topk:
+    :param min_avg_labels:
+    :param correlation_method:
+    :param threshold:
+    :return:
+    """
+    # mkdir the necessary folders
+    mkdir_necessary()
+    # remove the duplicate text
+    df_no_repeat = remove_duplicate(file_name)
+    # get the topk frequent labels
+    top_labels = get_frequent_labels(df_no_repeat, topk)
+    # the len(new_label_name) may be less than topk because all 0s column will be delected
+    y_train, new_label_name = split_data(df_no_repeat, top_labels, min_avg_labels)
+    show_y_train_info(y_train)
+    show_tree(y_train, new_label_name, threshold, len(new_label_name), correlation_method)
+
+
+def main():
+
+    # command-line parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", type=str, help="the file name of the csv file, no .csv is needed.")
+    parser.add_argument("topk", type=int,
+                        help="the number of the top frequent labels needed to be kept")
+    parser.add_argument("min_avg_labels", type=int,
+                        help="the minimum number of labels per instance should have. " +
+                             "instance with less than minimum num of labels will be delete")
+    parser.add_argument("--correlation_method", type=str, default="pearson", choices=["default", "cosine", "pearson"],
+                        help="three ways to calculate the correlation: default, cosine, pearson")
+    parser.add_argument("--threshold", type=int, default=0,
+                        help="three ways to calculate the correlation: default, cosine, pearson")
+    args = parser.parse_args()
+
+    data_clean(args.file_name, args.topk, args.min_avg_labels, args.correlation_method, args.threshold)
+
+
+if __name__ == "__main__":
+    main()
---
+++ enumeration.py
@@ -0,0 +1,203 @@
+import numpy as np
+import math
+from tqdm import tqdm
+import edge_threshold_adjust as adj
+import split
+import argparse
+import os
+
+
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        os.makedirs(path)
+        print("---  new folder...  ---")
+        print("---  OK  ---")
+    else:
+        print("---  There is this folder!  ---")
+
+
+def change_dim(nd_array):
+    if nd_array.ndim == 1:
+        return nd_array.reshape((nd_array.shape[0]), 1)
+    else:
+        return nd_array
+
+
+def bit_to_list(t, n):
+    """
+    convert an int to list
+    @param t: the int
+    @param n: the length of the bit
+    @return: a lit
+    """
+    bit_list = [0 for i in range(n)]
+    i = -1
+    while t != 0:
+        bit_list[i] = t % 2
+        t = t >> 1
+        i -= 1
+    return bit_list
+
+
+def get_candidate(list_length):
+    """
+    the length of the list
+    @param list_length:
+    @return: list contains all the possible
+    """
+    candidate = []
+    for i in range(pow(2, list_length)):
+        candidate.append(bit_to_list(i, list_length))
+    return candidate
+
+
+def transfer_label(trans_value):
+    """
+    trans_value is the possibility of 1 1, we need to get
+       0 1
+     0 x x
+     1 x x
+    """
+    label_trans_matrix = np.zeros((2, 2))
+    label_trans_matrix[0, 0] = -1000
+    label_trans_matrix[0, 1] = -1000
+    label_trans_matrix[1, 0] = -1000
+    label_trans_matrix[1, 1] = trans_value if trans_value != -1000 else -2000
+    return label_trans_matrix
+
+
+#  combine the result of each binary classifier
+def get_edge_matrix(link_list):
+    # get the matrix size
+    path = "./data/split_test/"
+    example = list(np.loadtxt(path + link_list[0]))
+    edge_matrix = np.zeros((len(example), len(link_list)))
+    for index in range(len(link_list)):
+        edge_column = list(np.loadtxt(path + link_list[index]))
+        edge_matrix[:, index] = edge_column
+    return edge_matrix
+
+
+def get_transfer_matrix(link_list, edge_list):
+    transfer_list = []
+    # convert the file to matrix
+    transfer_data = get_edge_matrix(link_list)
+    for line in transfer_data:
+        transfer_matrix = np.zeros((len(line) + 1, len(line) + 1))
+        line_index = 0
+        for edge in edge_list:
+            transfer_matrix[edge[0], edge[1]] = line[line_index]
+            transfer_matrix[edge[1], edge[0]] = line[line_index]
+            line_index += 1
+        transfer_list.append(transfer_matrix.copy())
+    return transfer_list
+
+
+def overall_optimal(status_list, transfer_matrix, edge_list):
+    score_list = []
+    all_possible = get_candidate(len(status_list))
+    for one_possible in all_possible:
+        score = 0
+        for index in range(len(one_possible)):
+            logpossibility = status_list[index] if one_possible[index] == 1 else np.log(1 - math.exp(status_list[index]))
+            score += logpossibility
+        # add the transfer possibility
+        for edge in edge_list:
+            label_trans_matrix = transfer_label(transfer_matrix[edge[0], edge[1]])
+            score += label_trans_matrix[one_possible[edge[0]], one_possible[edge[1]]]
+        score_list.append(score)
+    max_score = max(score_list)
+    max_index = score_list.index(max_score)
+    max_label = all_possible[max_index]
+    return max_score, max_label
+
+
+def exhust_search(filename, edge_list, left_num, link_path_list):
+    node_matrix = np.loadtxt("data/split_test/original_predict_" + str(left_num) + ".txt")
+    transfer_list = get_transfer_matrix(link_path_list, edge_list)
+    with open("data/enumeration/" + filename, "w") as vf:
+        iteration = 0
+        for index in tqdm(range(node_matrix.shape[0])):
+            node_matrix_log = np.zeros_like(node_matrix[index, :])
+            for i in range(len(node_matrix[index, :])):
+                node_matrix_log[i, ] = np.log(node_matrix[index, i])
+            transfer_list_log = np.zeros_like(transfer_list[index])
+            for i in range(transfer_list[index].shape[0]):
+                for j in range(transfer_list[index].shape[1]):
+                    if transfer_list[index][i, j] == 1:
+                        transfer_list_log[i, j] = 0
+                    else:
+                        transfer_list_log[i, j] = -1000
+            max_score, best_label = overall_optimal(node_matrix_log, transfer_list_log, edge_list)
+            s = ""
+            for i in range(len(best_label)):
+                if i != len(best_label) - 1:
+                    s += str(best_label[i])
+                    s += " "
+                else:
+                    s += str(best_label[i])
+            # if iteration < 2000:
+            vf.write(s + "\n")
+            # else:
+            #     break
+            # iteration += 1
+        print(index)
+
+
+def get_accuracy(result_file, true_file):
+    viterbi_result = np.array(np.loadtxt(result_file, dtype=int))
+    label = np.loadtxt(true_file, dtype=int)
+    # change dim
+    viterbi_result = change_dim(viterbi_result)
+    label = change_dim(label)
+    total_right = 0
+    for i in range(viterbi_result.shape[0]):
+        for j in range(viterbi_result.shape[1]):
+            if viterbi_result[i, j] == label[i, j]:
+                total_right += 1
+    return total_right / (label.shape[0] * label.shape[1])
+
+
+def inference(edge_list, total_num, upper_bound):
+    mkdir("data/enumeration")
+    adjust_num = int(np.floor(total_num * 0.3))
+    left_num = total_num - adjust_num
+    split.prepare_test_data(edge_list, total_num, adjust_num)
+    label_file = "data/split_test/y_test_" + str(left_num) + ".txt"
+    baseline_file = "data/split_test/original_predict_" + str(left_num) + "_round.txt"
+    if upper_bound == 1:
+        upper_bound_file = "data/enumeration/enumeration_upperbound.txt"
+        link_path_list = ["l" + str(edge[0]) + "_l" + str(edge[1]) + "_truth_" + str(left_num) + ".txt" for edge in edge_list]
+        exhust_search("enumeration_upperbound.txt", edge_list, left_num, link_path_list)
+        upper_bound_accuracy = get_accuracy(upper_bound_file, label_file)
+        print("the upper bound accuracy is: ")
+        print(upper_bound_accuracy)
+    else:
+        link_path_list = adj.return_file_list(edge_list, total_num, adjust_num)
+        exhust_search("enumeration.txt", edge_list, left_num, link_path_list)
+        predict_file = "data/enumeration/enumeration.txt"
+        predict_accuracy = get_accuracy(predict_file, label_file)
+        print("our method's accuracy is: ")
+        print(predict_accuracy)
+    baseline_accuracy = get_accuracy(baseline_file, label_file)
+    print("the baseline's accuracy is: ")
+    print(baseline_accuracy)
+
+
+def main():
+    # edge list, same in the store folder
+    edge_list = [(0, 1), (0, 2), (0, 3)]
+    # the total number of the text instance
+    total_num = 12
+    parser = argparse.ArgumentParser()
+    parser.add_argument("upper_bound", type=int, help="0: use the predicted edge info, 1: use the true edge info to" +
+                                                      "calculate the upper bound")
+    args = parser.parse_args()
+    inference(edge_list, total_num, args.upper_bound)
+    # inference(edge_list, total_num, 0)
+
+
+if __name__ == '__main__':
+    main()
+
